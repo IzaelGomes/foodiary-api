ee85e680c920ed763b964aa31e920f075e818058
Izael gomes <izaelgomes127@gmail.com>
2025-07-20 21:28:37 -0300

    feat: sign in and sign up controller

--- foodiary-api/src/controllers/signinController.ts
+++ foodiary-api/src/controllers/signinController.ts
@@ -1,6 +1,11 @@
 import { z } from 'zod';
 import { HttpRequest, HttpResponse } from '../types/https';
-import { badRequest, created, ok } from '../utils/http';
+import { badRequest, created, ok, unauthorized } from '../utils/http';
+import { db } from '../db';
+import { and, eq } from 'drizzle-orm';
+import { usersTable } from '../db/schema';
+import { compare } from 'bcryptjs';
+import { signAccessTokenFor } from '../lib/jwt';
 
 const schema = z.object({
   email: z.email(),
@@ -14,8 +19,29 @@ export class SignInController {
       return badRequest({ erros: error.issues });
     }
 
+    const user = await db.query.usersTable.findFirst({
+      columns: {
+        id: true,
+        email: true,
+        password: true,
+      },
+      where: and(eq(usersTable.email, data.email)),
+    });
+
+    if (!user) {
+      return unauthorized({ error: 'Invalid credentials' });
+    }
+
+    const isPasswordValid = await compare(data.password, user.password);
+
+    if (!isPasswordValid) {
+      return unauthorized({ error: 'Invalid credentials' });
+    }
+
+    const accessToken = signAccessTokenFor(user.id);
+
     return created({
-      data,
+      accessToken,
     });
   }
 }
--- foodiary-api/src/controllers/signupController.ts
+++ foodiary-api/src/controllers/signupController.ts
@@ -1,6 +1,11 @@
 import { z } from 'zod';
 import { HttpRequest, HttpResponse } from '../types/https';
-import { badRequest, created } from '../utils/http';
+import { badRequest, conflict, created } from '../utils/http';
+import { db } from '../db';
+import { usersTable } from '../db/schema';
+import { and, eq } from 'drizzle-orm';
+import { hash } from 'bcryptjs';
+import { calculateGoals } from '../lib/calculateGoals';
 
 const schema = z.object({
   goal: z.enum(['lose', 'maintain', 'gain']),
@@ -23,8 +28,40 @@ export class SignUpController {
       return badRequest({ erros: error.issues[0].path });
     }
 
+    const userAlreadyExists = await db.query.usersTable.findFirst({
+      columns: {
+        email: true,
+      },
+      where: and(eq(usersTable.email, data.account.email)),
+    });
+
+    if (userAlreadyExists) {
+      return conflict({ error: 'This email is already in use.' });
+    }
+
+    const goals = calculateGoals({
+      activityLevel: data.activityLevel,
+      birthDate: new Date(data.birthDate),
+      gender: data.gender,
+      goal: data.goal,
+      height: data.height,
+      weight: data.weight,
+    });
+
+    const hashedPassword = await hash(data.account.password, 8);
+
+    const [user] = await db
+      .insert(usersTable)
+      .values({
+        ...data,
+        ...goals,
+        ...data.account,
+        password: hashedPassword,
+      })
+      .returning({ id: usersTable.id });
+
     return created({
-      data,
+      userId: user.id,
     });
   }
 }
