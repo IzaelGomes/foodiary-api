da5a8ec38b95eebf5bb2203f6f8ee4f65ff474e0
Izael gomes <izaelgomes127@gmail.com>
2025-07-20 21:28:18 -0300

    feat: me route

---
+++ foodiary-api/src/functions/me.ts
@@ -0,0 +1,16 @@
+import { APIGatewayProxyEventV2 } from 'aws-lambda';
+
+import { unauthorized } from '../utils/http';
+import { parseProtectedEvent } from '../utils/parseProtectedEvent';
+import { parseResponse } from '../utils/parseResponse';
+import { MeController } from '../controllers/meController';
+
+export async function handler(event: APIGatewayProxyEventV2) {
+  try {
+    const request = parseProtectedEvent(event);
+    const response = await MeController.handle(request);
+    return parseResponse(response);
+  } catch {
+    return parseResponse(unauthorized({ error: 'Invalid access token.' }));
+  }
+}
